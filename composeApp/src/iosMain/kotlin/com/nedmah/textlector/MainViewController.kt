063957a2a012cd47c7e91002b8a3188247258d07
Sergei Veriaskin <34280229+Counserf@users.noreply.github.com>
2026-09-15 21:49:30 +0300

    feat: bridge opened files into Compose

--- composeApp/src/iosMain/kotlin/com/nedmah/textlector/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/nedmah/textlector/MainViewController.kt
@@ -1,11 +1,16 @@
 package com.nedmah.textlector
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.nedmah.textlector.common.platform.file.IncomingFileStore
 import com.nedmah.textlector.di.dataModule
 import com.nedmah.textlector.di.platformModule
 import org.koin.core.context.startKoin
 
 fun MainViewController() = ComposeUIViewController { App() }
+
+fun handleIncomingFile(path: String) {
+    IncomingFileStore.submit(path)
+}
 
 fun initKoin() {
     startKoin {
