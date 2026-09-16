365931673877df5555ec4beb3ee77eeea0a53c03
Sergei Veriaskin <34280229+Counserf@users.noreply.github.com>
2026-09-16 15:07:55 +0300

    feat: persist iOS crash diagnostics

--- composeApp/src/iosMain/kotlin/com/nedmah/textlector/common/platform/logging/CrashReporter.kt
+++ composeApp/src/iosMain/kotlin/com/nedmah/textlector/common/platform/logging/CrashReporter.kt
@@ -1,16 +1,18 @@
 package com.nedmah.textlector.common.platform.logging
 
 actual object CrashReporter {
-
     actual fun log(message: String, tag: String) {
-        // stub
+        TtsDiagnosticLog.append(tag, message)
     }
 
     actual fun recordException(e: Throwable, message: String) {
-        // stub
+        TtsDiagnosticLog.append(
+            "Exception",
+            "$message | ${e::class.simpleName}: ${e.message}\n${e.stackTraceToString()}"
+        )
     }
 
     actual fun setKey(key: String, value: String) {
-        // stub
+        TtsDiagnosticLog.append("Context", "$key=$value")
     }
 }
