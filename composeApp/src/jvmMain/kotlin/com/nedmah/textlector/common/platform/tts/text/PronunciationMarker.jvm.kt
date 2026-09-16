717a914d622b793c9fc81ed4e994f9e062844e74
Sergei Veriaskin <34280229+Counserf@users.noreply.github.com>
2026-09-16 10:41:10 +0300

    feat: add JVM pronunciation marker

---
+++ composeApp/src/jvmMain/kotlin/com/nedmah/textlector/common/platform/tts/text/PronunciationMarker.jvm.kt
@@ -0,0 +1,8 @@
+package com.nedmah.textlector.common.platform.tts.text
+
+actual class PronunciationMarker actual constructor() {
+    private val common = NeuralTextPreprocessor()
+
+    actual suspend fun prepare(text: String, language: String): String =
+        common.process(text, language)
+}
