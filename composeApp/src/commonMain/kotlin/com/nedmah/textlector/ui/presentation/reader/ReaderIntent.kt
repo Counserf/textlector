a82a625c1a92624d7b8773964bc9da3d206ff79c
Sergei Veriaskin <34280229+Counserf@users.noreply.github.com>
2026-09-16 10:44:20 +0300

    feat: add audio pre-generation action

--- composeApp/src/commonMain/kotlin/com/nedmah/textlector/ui/presentation/reader/ReaderIntent.kt
+++ composeApp/src/commonMain/kotlin/com/nedmah/textlector/ui/presentation/reader/ReaderIntent.kt
@@ -1,8 +1,8 @@
 package com.nedmah.textlector.ui.presentation.reader
 
-
 sealed interface ReaderIntent {
     data object NavigateBack : ReaderIntent
+    data object StartAudioGeneration : ReaderIntent
 
     data class LoadDocument(val docId : String) : ReaderIntent
     data class ToggleFavorite(val docId : String, val isFavorite: Boolean) : ReaderIntent
