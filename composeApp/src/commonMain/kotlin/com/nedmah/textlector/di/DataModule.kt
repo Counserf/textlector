69822c67f28e2b09e8d8b8176a0ec9f83629b3af
Sergei Veriaskin <34280229+Counserf@users.noreply.github.com>
2026-09-16 10:56:31 +0300

    fix: inject background processor into import flow

--- composeApp/src/commonMain/kotlin/com/nedmah/textlector/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/nedmah/textlector/di/DataModule.kt
@@ -90,7 +90,7 @@ val dataModule = module {
         ReaderViewModel(get(), get(), get(), get(), get())
     }
     factory {
-        ImportViewModel(get(), get(), get(), get(), get(), get(), get())
+        ImportViewModel(get(), get(), get(), get(), get(), get(), get(), get())
     }
     factory {
         SettingsViewModel(get(), get(), get(), get(), get(), get(), get())
